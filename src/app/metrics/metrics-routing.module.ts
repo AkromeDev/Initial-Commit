137613c4d207beb6dd69cdb2a2a7027843b01b37
JCBackendDev <jcbackenddev@gmail.com>
2021-12-25 23:32:42 +0100

    added metrics path showinf MetricsHome comp
    And this is workinf of course. It is crazy that I did not already know that!

--- src/app/metrics/metrics-routing.module.ts
+++ src/app/metrics/metrics-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MetricsHomeComponent } from './metrics-home/metrics-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'metrics', component: MetricsHomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
